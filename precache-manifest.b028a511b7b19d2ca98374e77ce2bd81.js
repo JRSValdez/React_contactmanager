fcfa69cfc0e08420facfda8690a642fe31df732b
jrsvaldez <jrsifontes16@gmail.com>
2018-12-16 21:45:41 -0600

    Updates

---
+++ precache-manifest.b028a511b7b19d2ca98374e77ce2bd81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6f030001e4c8e6469ab",
+    "url": "/React_contactmanager/static/js/runtime~main.e6f03000.js"
+  },
+  {
+    "revision": "7fe6ee53a463f6e1e775",
+    "url": "/React_contactmanager/static/js/main.7fe6ee53.chunk.js"
+  },
+  {
+    "revision": "d75c3c3940eff4ff8788",
+    "url": "/React_contactmanager/static/js/1.d75c3c39.chunk.js"
+  },
+  {
+    "revision": "7fe6ee53a463f6e1e775",
+    "url": "/React_contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "d75c3c3940eff4ff8788",
+    "url": "/React_contactmanager/static/css/1.703b4106.chunk.css"
+  },
+  {
+    "revision": "a78c3072aa660856292edf88b78d5f3b",
+    "url": "/React_contactmanager/index.html"
+  }
+];
